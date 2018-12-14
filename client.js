3cd6145cd923c8aadbb36e31820d9d63b0c4b2b1
Dion Roloff <dion.roloff@gmail.com>
2018-12-14 16:54:43 -0600

    completed base functionality

---
+++ client.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    $('#submitButton').on('click', submitEmployeeInfo);
+    $('#submitButton').on('click', calculateCosts);
+    $('#submitButton').on('click', function() {
+        $('#firstName').val(``);
+        $('#lastName').val(``);
+        $('#iDNumber').val(``);
+        $('#jobTitle').val(``);
+        $('#annualSalary').val(``);
+    });
+})
+
+
+
+function submitEmployeeInfo() {
+    let firstName = $('#firstName').val();
+    let lastName = $('#lastName').val();
+    let iDNumber = $('#iDNumber').val();
+    let jobTitle = $('#jobTitle').val();
+    let annualSalary = $('#annualSalary').val();
+
+    
+    $('.employeeListForm').append('<li>Full Name: ' + firstName + ' ' + lastName + '</li>');
+    $('.employeeListForm').append('<li>ID Number: ' + iDNumber + '</li>');
+    $('.employeeListForm').append('<li>Title: ' + jobTitle + '</li>');
+    $('.employeeListForm').append('<li>Salary: ' + annualSalary + '</li>');
+    $('.employeeListForm').append('<br>');
+    
+    // $( '#firstName' ).each(function(){
+    //     this.reset();
+    // });
+    
+}
+
+function calculateCosts() {
+
+    let monthlySalary = $('#annualSalary').val() / 12;
+   
+    $('.monthlyCosts').html('<h2>Monthly Costs: $' + monthlySalary + '</h2>')
+
+    if (monthlySalary > 20000) {
+        $('.monthlyCosts').css('color', 'red');
+    }
+}
