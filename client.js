d5be8872907f62802b4bd6f4612ab3140b3fc972
Dion Roloff <dion.roloff@gmail.com>
2018-12-18 18:25:29 -0600

    updated assignment to complete base goals

--- client.js
+++ client.js
@@ -1,43 +1,82 @@
-$(document).ready(function () {
-    $('#submitButton').on('click', submitEmployeeInfo);
-    $('#submitButton').on('click', calculateCosts);
-    $('#submitButton').on('click', function() {
-        $('#firstName').val(``);
-        $('#lastName').val(``);
-        $('#iDNumber').val(``);
-        $('#jobTitle').val(``);
-        $('#annualSalary').val(``);
-    });
-    
-    $('#deleteEmployee').on('click', function () {
-        
-        $('#fN').empty();
-        $('#iD').empty();
-        $('#jT').empty();
-        $('#aS').empty();
-        
-    })
-    
-})
+$(document).ready(readyNow);
 
-
-
-function submitEmployeeInfo() {
-    let firstName = $('#firstName').val();
-    let lastName = $('#lastName').val();
-    let iDNumber = $('#iDNumber').val();
-    let jobTitle = $('#jobTitle').val();
-    let annualSalary = $('#annualSalary').val();
-
-    
-    $('.employeeListForm').append('<li id="fN">Full Name: ' + firstName + ' ' + lastName + '</li>');
-    $('.employeeListForm').append('<li id="iD">ID Number: ' + iDNumber + '</li>');
-    $('.employeeListForm').append('<li id="jT">Title: ' + jobTitle + '</li>');
-    $('.employeeListForm').append('<li id="aS">Salary: ' + annualSalary + '</li>');
-    $('.employeeListForm').append('<br>');
-    
-
+function readyNow() {
+    $('#submit').on('click', addEmployee);
+    $('#employee-list').on('click', '.delete', deleteEmployee);
+    $('#monthly-total').on('click', resolveConflict);
 }
+
+
+let totalMonthlySalary = 0;
+const MAX_MONTHLY_SALARY = 20000;
+const RED = 'rgb(255, 0, 0)';
+
+class Employee {
+    constructor(first, last, employeeId, title, salary) {
+        this.first = first;
+        this.last = last;
+        this.employeeId = employeeId;
+        this.title = title;
+        this.salary = salary;
+    }
+ 
+    toHTML() {
+        return `<tr>
+                    <td>${this.first}</td>
+                    <td>${this.last}</td>
+                    <td>${this.employeeId}</td>
+                    <td>${this.salary}</td>
+                    <td>${this.title}</td>
+                    <td>
+                        <button class="delete">
+                            Delete
+                        </button>
+                    </td>
+                </tr>`
+    } 
+} 
+
+
+function deleteEmployee() {
+    
+    let deletedEmployeeSalary = $(this).parent().prev().prev().text();   
+
+    totalMonthlySalary -= (parseInt(deletedEmployeeSalary) / 12).toFixed(2);
+    $(this).parent().parent().remove(); //removes the tr
+    $('#monthly-total').html(totalMonthlySalary);
+
+    if (totalMonthlySalary > MAX_MONTHLY_SALARY) {
+        $('#monthly-total').css('color', 'red');
+    } else {
+        $('#monthly-total').css('color', 'black');
+    }
+
+} 
+
+function addEmployee() {
+
+    
+    let salary = $('#salary').val(); 
+    let firstName = $('#first-name').val();
+    let lastName = $('#last-name').val();
+    let employeeId = $('#employee-id').val();
+    let title = $('#title').val();
+    const employeeToAdd = new Employee(firstName, lastName, employeeId, title, salary);
+    $('#employee-list').append(employeeToAdd.toHTML());
+
+    
+    let monthlySalary = parseInt(salary) / 12;
+    totalMonthlySalary += monthlySalary;
+   
+    $('#monthly-total').html(totalMonthlySalary);
+
+    if (totalMonthlySalary > MAX_MONTHLY_SALARY) {
+        $('#monthly-total').css('color', 'red');
+    } else {
+        $('#monthly-total').css('color', 'black');
+    }
+
+} 
 
 
 function calculateCosts() {
