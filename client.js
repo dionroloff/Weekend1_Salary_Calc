e9143d6926c03aa5deaaf694263b7f9dd068323d
Dion Roloff <dion.roloff@gmail.com>
2018-12-19 13:32:21 -0600

    delete button functioning

--- client.js
+++ client.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
     $('#submitButton').on('click', submitEmployeeInfo);
     
-    $('#submitButton').on('click', calculateCosts);
+    // $('#submitButton').on('click', calculateCosts);
     
 
-    $('.employeeListForm').on('click', '.delete', function() {
+    $('#employee-list').on('click', '.delete', function() {
         console.log(this);
-        
+        let deletedEmployeeSalary = $(this).parent().prev().prev().text();   
+        $(this).parent().parent().remove(); //removes the tr
     });
     
     
@@ -17,6 +18,15 @@ $(document).ready(function () {
 
 //collects an employee's information from the input fields and appends it to the DOM
 function submitEmployeeInfo() {
+
+    let monthlySalary = ($('#annualSalary').val() / 12).toFixed(2);
+    $('.monthlyCosts').html(`<h2>Monthly Costs: ${monthlySalary}</h2>`);
+
+    if (monthlySalary > 20000) {
+        $('.monthlyCosts').css('color', 'red');
+        
+    } 
+
     //assigning input values to variables
     let firstName = $('#firstName').val();
     let lastName = $('#lastName').val();
@@ -25,13 +35,17 @@ function submitEmployeeInfo() {
     let annualSalary = $('#annualSalary').val();
 
     //appends the input fields to the DOM using variables above
-    $('.employeeListForm').append(`<div id="full-name">Full Name: ${firstName} ${lastName}</div><br>`);
-    $('.employeeListForm').append(`<div id="id-number">ID Number: ${iDNumber}</div><br>`);
-    $('.employeeListForm').append(`<div id="job-title">Job Title ${jobTitle}</div><br>`);
-    $('.employeeListForm').append(`<div id="salary">Salary: ${annualSalary}</div><br>`);
-    $('.employeeListForm').append(`<button class="delete">
-                                       Delete
-                                   </button><br>`);
+    let append = $(`<tr>
+                <td id="first-name">${firstName}</td>
+                <td id="last-name">${lastName}<td>
+                <td id="id-number">${iDNumber}</td>
+                <td id="salary">${annualSalary}</td>
+                <td id="job-title">${jobTitle}</td>
+                
+                <td><button class="delete">Delete</button></td>
+                </tr>`)
+    $('#employee-list').append(append);
+    
     //clears the input fields after submit button clicked
     $('#firstName').val(``);
     $('#lastName').val(``);
@@ -45,13 +59,13 @@ function submitEmployeeInfo() {
 
 
 //calculates the monthly cost of employee slaries
-function calculateCosts() {
+// function calculateCosts() {
     
-    let monthlySalary = ($('#annualSalary').val() / 12).toFixed(2);
-    $('.monthlyCosts').html('<h2>Monthly Costs: $' + monthlySalary + '</h2>')
+//     let monthlySalary = ($('#annualSalary').val() / 12).toFixed(2);
+//     $('.monthlyCosts').html('<h2>Monthly Costs: $' + monthlySalary + '</h2>')
 
-    if (monthlySalary > 20000) {
-        $('.monthlyCosts').css('color', 'red');
+//     if (monthlySalary > 20000) {
+//         $('.monthlyCosts').css('color', 'red');
         
-    } 
-}
+//     } 
+// }
